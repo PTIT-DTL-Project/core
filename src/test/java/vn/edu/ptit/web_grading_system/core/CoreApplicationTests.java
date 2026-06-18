a674ffec04420edf2e0f2d186ba10d47e7b2a40e
Duong Vu Cong Tuan <vucongtuanduong2409@gmail.com>
2026-06-18 11:18:49 +0700

    first init project

---
+++ src/test/java/vn/edu/ptit/web_grading_system/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.ptit.web_grading_system.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
